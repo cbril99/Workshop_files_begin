64b56f2f68d76aed1ba9a941e9ce596f3ece65c3
cbril99 <cbril99@users.noreply.github.com>
2021-06-03 17:20:55 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -7,18 +7,10 @@ var myOtherBox = document.getElementById('myOtherBox');
 function spin(){
     myOtherBox.object3D.rotation.x += rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.x);
- }
 
- setInterval(spin, 16); //equivalent to 60 fps
-
- function spin(){
     myOtherBox.object3D.rotation.y += rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.y);
- }
 
- setInterval(spin, 16); //equivalent to 60 fps
-
- function spin(){
     myOtherBox.object3D.rotation.z += rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.z);
  }
