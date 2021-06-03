7555e9e4f44fd6e97503969525b3140f14869973
cbril99 <cbril99@users.noreply.github.com>
2021-06-03 17:18:17 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -10,3 +10,17 @@ function spin(){
  }
 
  setInterval(spin, 16); //equivalent to 60 fps
+
+ function spin(){
+    myOtherBox.object3D.rotation.y += rotationSpeed;
+ 	console.log(myOtherBox.object3D.rotation.y);
+ }
+
+ setInterval(spin, 16); //equivalent to 60 fps
+
+ function spin(){
+    myOtherBox.object3D.rotation.z += rotationSpeed;
+ 	console.log(myOtherBox.object3D.rotation.z);
+ }
+
+ setInterval(spin, 16); //equivalent to 60 fps
